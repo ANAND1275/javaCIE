dbde49a09309cea0916b2c736ce40570012ba0cd
ANAND1275 <2005anandpatil@gmail.com>
2025-04-09 16:08:07 +0530

    save to main

---
+++ Datatypes.java
@@ -0,0 +1,19 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        byte a = 10;
+        short b = 200;
+        int c = 1000;
+        float d = 10.0f;
+        double e = 10.001;
+        char f = 'h';
+        boolean g = true;
+
+        System.out.println("byte = " + a);
+        System.out.println("short = " + b);
+        System.out.println("int = " + c);
+        System.out.println("float = " + d);
+        System.out.println("double = " + e);
+        System.out.println("char = " + f);
+        System.out.println("boolean = " + g);
+    }
+}
